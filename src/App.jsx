d50614fd832cbc3756e7d17e890da1e95104ff02
unknown <Administrador@rtrsestec236139.corp.udesc.br>
2024-11-01 16:57:19 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className="container">
+      <nav>Navbar</nav>
+      <main>Main</main>
+      <div id="sidebar">Sidebar</div>
+      <div id="content1">Content1</div>
+      <div id="content2">Content2</div>
+      <div id="content3">Content3</div>
+      <footer>Footer</footer>
+    </div>
+    </>
+  )
+}
+
+export default App
